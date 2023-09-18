11e3106df04f27edc8dfc52cde9af3fa9b10d0c1
akbarahmadjonov <akhmadjonovakbardeveloper@gmail.com>
2023-09-17 22:17:21 -0700

    Initial commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,15 @@
+import { Routes } from "react-router-dom";
+// import ScrollToTop from "./../shared/components/index";
+
+// Import pages
+
+const Routings = () => {
+  return (
+    <>
+      {/* <ScrollToTop /> */}
+      <Routes></Routes>
+    </>
+  );
+};
+
+export default Routings;
