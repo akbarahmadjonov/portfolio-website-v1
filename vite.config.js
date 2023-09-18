650b71b813089a1138d2a716307260f7c1d34fd7
akbarahmadjonov <akhmadjonovakbardeveloper@gmail.com>
2023-09-18 02:01:32 -0700

    Mainly worked on installing and configuring Typescript

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": resolve("src"),
+      "@components": resolve("src", "widgets"),
+      "@pages": resolve("src", "pages"),
+      "@images": resolve("src", "shared", "assets", "images"),
+      "@api": resolve("src", "Api"),
+      "@apps": resolve("src", "apps"),
+    },
+  },
+});
