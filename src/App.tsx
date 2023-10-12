fc5b38f0ac401dfde17adc1cbaf4345e1c6f8796
akbarahmadjonov <akhmadjonovakbardeveloper@gmail.com>
2023-10-12 07:07:51 -0700

    Worked on contact, education, experience and portfolio pages, and added communication

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Routings from "@pages/index";
+// ANT DESIGN
+import { LoadingOutlined } from "@ant-design/icons";
+import { Spin } from "antd";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1100);
+  }, []);
+
+  if (isLoading) {
+    const antIcon = (
+      <LoadingOutlined style={{ fontSize: 60, color: "#525252" }} spin />
+    );
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "300px",
+        }}
+        className="loading-container"
+      >
+        <Spin indicator={antIcon} />
+        <p style={{ fontSize: "20px", color: "#525252" }}>
+          Welcome back, thanks for visiting!
+        </p>
+      </div>
+    );
+  }
+
   return (
     <>
       <Routings />
