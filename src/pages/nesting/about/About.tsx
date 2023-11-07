348db5019d11bf2929f4beee54884616fcd38037
akbarahmadjonov <akhmadjonovakbardeveloper@gmail.com>
2023-11-07 14:43:22 +0500

    Updated changes

--- src/pages/nesting/about/About.tsx
+++ src/pages/nesting/about/About.tsx
@@ -27,27 +27,27 @@ export const About = () => {
             Lives in Tashkent, Uzbekistan
           </span>
           <span className="about__parts">
-            <FaInfoCircle className="about__icon" size={14} /> Birthday: July
-            15, 2002
+            <FaInfoCircle className="about__icon" size={14} /> Birthday: July 15
           </span>
-          <span className="about__parts">
+          {/* <span className="about__parts">
             <FaInfoCircle className="about__icon" size={14} /> Experience: Nearly 1
             Year
-          </span>
+          </span> */}
         </div>
         <div className="about__selfSectionWrapper">
           <p className="about__myselfSection">
             Hello World! My full name is Akbar Ahmadjonov. I am both frontend
-            and backend developer with almost one years of web experience building websites. I
-            am passionate about web development in all forms and like building
-            apps that can help others. I have created - and improved - dozens of
-            successful web applications/websites that have reached 200 users.
-            And I have worked with individuals and companies. I maintain an
-            unwavering dedication to continuous self-improvement. I continually
-            expand my skill set by exploring diverse platforms, frameworks, and
-            programming languages. Outside of work I am constantly teaching
-            myself and learning how to build apps with various platforms,
-            frameworks, languages, etc. My other hobbies include: Robotics, Math, checkers, and more!
+            and backend developer with almost one years of web experience
+            building websites. I am passionate about web development in all
+            forms and like building apps that can help others. I have created -
+            and improved - dozens of successful web applications/websites that
+            have reached 200 users. And I have worked with individuals and
+            companies. I maintain an unwavering dedication to continuous
+            self-improvement. I continually expand my skill set by exploring
+            diverse platforms, frameworks, and programming languages. Outside of
+            work I am constantly teaching myself and learning how to build apps
+            with various platforms, frameworks, languages, etc. My other hobbies
+            include: Robotics, Math, checkers, and more!
           </p>
         </div>
         {/* SKILLS SECTION */}
